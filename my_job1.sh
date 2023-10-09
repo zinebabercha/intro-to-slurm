7b20e1517f12746730c8c09ac8cae3394bdb953e
zineb abercha <zineb.abercha@frontend02.cm.cluster>
2023-10-09 12:40:27 +0100

    initila commit

---
+++ my_job1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --job-name=my_job 
+#SBATCH --output=output.txt
+#SBATCH --error=error.log
+#SBATCH --ntasks=1
+#SBATCH --time=00:01:00
+#SBATCH --partition=defq 
+echo "Hello, SLURM!"
